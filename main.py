284920cee0f4bf3ff10c245f6f069f5bf172c96e
DiegoCaroFrechilla <146827294+DiegoCaroFrechilla@users.noreply.github.com>
2024-10-28 16:29:50 +0100

    tomas molina da goat 🐐

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+def on_forever():
+    temperature = input.temperature()
+    led.plot_bar_graph(temperature, 50)
 
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
-
+basic.forever(function on_forever() {
+    let temperature = input.temperature()
+    led.plotBarGraph(temperature, 50)
+})
