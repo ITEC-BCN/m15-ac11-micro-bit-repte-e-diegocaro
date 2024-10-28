34e80f53ef7f0f61472ac34eac0628a51cb06d98
DiegoCaroFrechilla <146827294+DiegoCaroFrechilla@users.noreply.github.com>
2024-10-28 16:32:12 +0100

    tomas molina da goat 2🐐🐐

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+x = 2
+y = 2
+def on_forever2():
+    global x,y
+    led.plot(x, y)
+    basic.pause(50)
+    basic.clear_screen()
+    accX = input.acceleration(Dimension.X)
+    accY = input.acceleration(Dimension.Y)
+
+    if(accX <= 150 and  x>0):
+        x = x-1
+    if(accX>150 and x<4):
+        x=x+1
+    if(accY<= 150 and y>0):
+        y=y-1
+    if(accY> 150 and y<4):
+        y=y+1
+basic.forever(on_forever2)
+
+"""
 def on_forever():
     temperature = input.temperature()
     led.plot_bar_graph(temperature, 50)
 
 basic.forever(on_forever)
+"""
--- main.ts
+++ main.ts
@@ -1,4 +1,34 @@
-basic.forever(function on_forever() {
-    let temperature = input.temperature()
-    led.plotBarGraph(temperature, 50)
+let x = 2
+let y = 2
+basic.forever(function on_forever2() {
+    
+    led.plot(x, y)
+    basic.pause(50)
+    basic.clearScreen()
+    let accX = input.acceleration(Dimension.X)
+    let accY = input.acceleration(Dimension.Y)
+    if (accX <= 150 && x > 0) {
+        x = x - 1
+    }
+    
+    if (accX > 150 && x < 4) {
+        x = x + 1
+    }
+    
+    if (accY <= 150 && y > 0) {
+        y = y - 1
+    }
+    
+    if (accY > 150 && y < 4) {
+        y = y + 1
+    }
+    
 })
+/** 
+def on_forever():
+    temperature = input.temperature()
+    led.plot_bar_graph(temperature, 50)
+
+basic.forever(on_forever)
+
+ */
