0c5f561d80b433ffc3c71366daa81317c465daea
raimonizard <97727989+raimonizard@users.noreply.github.com>
2024-11-25 20:46:48 +0100

    feat: add button A functionallity

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
+temp = False
 x = 2
 y = 2
-def on_forever2():
-    global x,y
-    led.plot(x, y)
-    basic.pause(50)
-    basic.clear_screen()
-    accX = input.acceleration(Dimension.X)
-    accY = input.acceleration(Dimension.Y)
 
-    if(accX <= 150 and  x>0):
-        x = x-1
-    if(accX>150 and x<4):
-        x=x+1
-    if(accY<= 150 and y>0):
-        y=y-1
-    if(accY> 150 and y<4):
-        y=y+1
+def on_button_pressed_a():
+    global temp
+    if (temp):
+        temp = False
+    else:
+        temp = True
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_forever2():
+    global temp
+    if (temp == False):
+        global x,y
+        led.plot(x, y)
+        basic.pause(50)
+        basic.clear_screen()
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+
+        if(accX <= 150 and  x>0):
+            x = x-1
+        if(accX>150 and x<4):
+            x=x+1
+        if(accY<= 150 and y>0):
+            y=y-1
+        if(accY> 150 and y<4):
+            y=y+1
 basic.forever(on_forever2)
 
-"""
 def on_forever():
-    temperature = input.temperature()
-    led.plot_bar_graph(temperature, 50)
-
+    global temp
+    if (temp):
+        temperature = input.temperature()
+        led.plot_bar_graph(temperature, 50)
 basic.forever(on_forever)
-"""
--- main.ts
+++ main.ts
@@ -1,34 +1,51 @@
+let temp = false
 let x = 2
 let y = 2
-basic.forever(function on_forever2() {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
-    led.plot(x, y)
-    basic.pause(50)
-    basic.clearScreen()
-    let accX = input.acceleration(Dimension.X)
-    let accY = input.acceleration(Dimension.Y)
-    if (accX <= 150 && x > 0) {
-        x = x - 1
-    }
-    
-    if (accX > 150 && x < 4) {
-        x = x + 1
-    }
-    
-    if (accY <= 150 && y > 0) {
-        y = y - 1
-    }
-    
-    if (accY > 150 && y < 4) {
-        y = y + 1
+    if (temp) {
+        temp = false
+    } else {
+        temp = true
     }
     
 })
-/** 
-def on_forever():
-    temperature = input.temperature()
-    led.plot_bar_graph(temperature, 50)
-
-basic.forever(on_forever)
-
- */
+basic.forever(function on_forever2() {
+    let accX: number;
+    let accY: number;
+    
+    if (temp == false) {
+        
+        led.plot(x, y)
+        basic.pause(50)
+        basic.clearScreen()
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+        if (accX <= 150 && x > 0) {
+            x = x - 1
+        }
+        
+        if (accX > 150 && x < 4) {
+            x = x + 1
+        }
+        
+        if (accY <= 150 && y > 0) {
+            y = y - 1
+        }
+        
+        if (accY > 150 && y < 4) {
+            y = y + 1
+        }
+        
+    }
+    
+})
+basic.forever(function on_forever() {
+    let temperature: number;
+    
+    if (temp) {
+        temperature = input.temperature()
+        led.plotBarGraph(temperature, 50)
+    }
+    
+})
